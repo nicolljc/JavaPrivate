3593ff9e3b22aa3c081b0a63bea30a5cb1414398
nicolljc <jesse.nicoll@fountainheadcollege.com>
2017-05-04 20:28:33 -0400

    -a

---
+++ Chapter2/Chapter2Examples/Success.java
@@ -0,0 +1,12 @@
+// It's a mad, mad program.
+
+public class Success
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Success\n");
+		System.out.print("Success ");
+		System.out.print("Success\n");
+		System.out.println("\nSuccess");
+	}
+}
