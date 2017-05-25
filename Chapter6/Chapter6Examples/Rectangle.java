2a0d909e3fede2dc0494758e44126fedf9f1a4a5
nicolljc <jesse.nicoll@fountainheadcollege.com>
2017-05-24 20:38:06 -0400

    -a

--- Chapter6/Chapter6Examples/Rectangle.java
+++ Chapter6/Chapter6Examples/Rectangle.java
@@ -1,12 +1,23 @@
 /**
-	Rectangle class, phase 1
-	Under construction!
+	Rectangle class, phase 5
 */
 
 public class Rectangle
 {
 	private double length;
 	private double width;
+	
+	/**
+		Constructor
+		@param len The length of the rectangle.
+		@param w The width of the rectangle.
+	*/
+	
+	public Rectangle(double len, double w)
+	{
+		length = len;
+		width = w;
+	}
 	
 	/**
 		The setLength method stores a value in the
