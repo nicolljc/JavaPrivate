4ad38ca9ba4dd3315a8042384b5084ce917463ef
nicolljc <jesse.nicoll@fountainheadcollege.com>
2017-06-01 18:08:09 -0400

    -a

--- Chapter8/Chapter8Examples/Stock.java
+++ Chapter8/Chapter8Examples/Stock.java
@@ -54,4 +54,14 @@ public class Stock
 		// Return the string.
 		return str;
 	}
+	
+	public Stock copy()
+	{
+		// Create a new Stock object and initialize it
+		// with the same data held by the calling object.
+		Stock copyObject = new Stock(symbol, sharePrice);
+		
+		// Return a reference to the new object.
+		return copyObject;
+	}
 }
